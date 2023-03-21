fc778e22a5c0c0dba4b73a34ffecec397a5c8fa2
name <121846883+A1azar1@users.noreply.github.com>
2023-03-21 11:34:31 -0500

    REST Client Initial commit

---
+++ BadgeAndMembershipRESTClient/src/test/java/edu/miu/badge/client/BadgeAndMembershipRestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.badge.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadgeAndMembershipRestClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
